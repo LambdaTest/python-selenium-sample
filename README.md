ece926f3c86fe3cae14dcd012473cbb714bb3c31
nikhil-lambda <nikhily@lambdatest.com>
2019-01-04 21:45:25 +0530

    python LT sample test

---
+++ README.md
@@ -0,0 +1,27 @@
+## Python unittest automation Lambdatest
+=======================
+
+Python selenium automation sample test for Lambdatest Cloud GRID.
+
+
+## Install Python
+ - Download the latest python build from https://www.python.org/downloads/
+ - Make sure pip should installed. you can check using `pip --version`
+
+
+### configuring test.
+- Replace {username}  with your username 
+- Replace {accessToken}  with your username 
+- List of supported platfrom, browser, version can be found at https://www.lambdatest.com/capabilities-generator/
+
+
+### Installating dependencies.
+```bash
+pip install selenium
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"   //Disable ssl warning
+```
+
+### Executing test
+```bash
+python google-serach-lambdatest.py
+```
---
+++ google-serach-lambdatest.py
@@ -0,0 +1,94 @@
+"""
+LambdaTest selenium automation sample example
+Configuration
+----------
+username: Username can be found at automation dashboard
+accessToken:  AccessToken can be genarated from automation dashboard or profile section
+
+Result
+-------
+Execute Test on lambdatest Distributed Grid perform selenium automation based 
+"""
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+
+class LTAutomate(unittest.TestCase):
+
+    """
+    Setup remote driver
+    Params
+    ----------
+    platfrom : Supported platfrom - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
+    browserName : Supported platfrom - (chrome, firefox, Internet Explorer, MicrosoftEdge)
+    version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
+
+    Result
+    -------
+    """
+    def setUp(self):
+        # username: Username can be found at automation dashboard
+        username="{username}"  
+        # accessToken:  AccessToken can be genarated from automation dashboard or profile section
+        accessToken="{accessToken}"
+        # gridUrl: gridUrl can be found at automation dashboard
+        gridUrl = "beta-hub.lambdatest.com/wd/hub"
+        
+        desired_cap = {
+            'platform' : "win10", 
+            'browserName' : "chrome",
+            'version' :  "67.0",
+            # Resolution of machine
+            "resolution": "1024x768", 
+            "name": "LambdaTest python google search test ",
+            "build": "LambdaTest python google search build",
+            "network": True,
+            "video": True,
+            "visual": True,
+            "console": True,
+        }
+
+        # URL: https://{username}:{accessToken}@beta-hub.lambdatest.com/wd/hub
+        url = "https://"+username+":"+accessToken+"@"+gridUrl
+        
+        print("Initiating remote driver on platfrom: "+desired_cap["platform"]+" browser: "+desired_cap["browserName"]+" version: "+desired_cap["version"])
+        self.driver = webdriver.Remote(
+            desired_capabilities=desired_cap,
+            command_executor= url
+        )
+
+    """
+    Setup remote driver
+    Params
+    ----------
+    Execute test:  navigate google.com search LambdaTest
+    Result
+    -------
+    print title
+    """
+    def test_search_in_google(self):
+        driver = self.driver
+        print("Driver initiated sucessfully.  Navigate url")
+        driver.get("https://www.google.com/ncr")
+
+        print("Searching lambdatest on google.com ")
+        time.sleep(8)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("lambdatest.com")
+        elem.submit()
+
+        print("Printing title of current page :"+driver.title)
+        driver.execute_script("lambda-status=passed")
+        print("Requesting to mark test : pass")
+
+    """
+    Quit selenium driver
+    """
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
