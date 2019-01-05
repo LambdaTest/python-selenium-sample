cc0ebfb0cfefbe3a66f30ec0d644fdf0c5a1b10b
nikhil-lambda <nikhily@lambdatest.com>
2019-01-05 18:09:35 +0530

    commenting updated.

--- google-serach-lambdatest.py
+++ google-serach-lambdatest.py
@@ -1,37 +1,39 @@
-"""
-LambdaTest selenium automation sample example
-Configuration
-----------
-username: Username can be found at automation dashboard
-accessToken:  AccessToken can be generated from automation dashboard or profile section
-
-Result
--------
-Execute Python Automation Tests on LambdaTest Distributed Selenium Grid 
-"""
 import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class LTAutomate(unittest.TestCase):
 
+class LTAutomate(unittest.TestCase):
     """
-    Setup remote driver
-    Params
+    LambdaTest selenium automation sample example
+    Configuration
     ----------
-    platform : Supported platform - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
-    browserName : Supported platform - (chrome, firefox, Internet Explorer, MicrosoftEdge)
-    version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
+    username: Username can be found at automation dashboard
+    accessToken:  AccessToken can be genarated from automation dashboard or profile section
 
     Result
     -------
+    Execute Test on lambdatest Distributed Grid perform selenium automation based 
     """
+
+    
     def setUp(self):
+        """
+        Setup remote driver
+        Params
+        ----------
+        platfrom : Supported platfrom - (Windows 10, Windows 8.1, Windows 8, Windows 7,  macOS High Sierra, macOS Sierra, OS X El Capitan, OS X Yosemite, OS X Mavericks)
+        browserName : Supported platfrom - (chrome, firefox, Internet Explorer, MicrosoftEdge)
+        version :  Supported list of version can be found at https://www.lambdatest.com/capabilities-generator/
+
+        Result
+        -------
+        """
         # username: Username can be found at automation dashboard
         username="{username}"  
-        # accessToken:  AccessToken can be generated from automation dashboard or profile section
+        # accessToken:  AccessToken can be genarated from automation dashboard or profile section
         accessToken="{accessToken}"
         # gridUrl: gridUrl can be found at automation dashboard
         gridUrl = "beta-hub.lambdatest.com/wd/hub"
@@ -53,24 +55,25 @@ class LTAutomate(unittest.TestCase):
         # URL: https://{username}:{accessToken}@beta-hub.lambdatest.com/wd/hub
         url = "https://"+username+":"+accessToken+"@"+gridUrl
         
-        print("Initiating remote driver on platform: "+desired_cap["platform"]+" browser: "+desired_cap["browserName"]+" version: "+desired_cap["version"])
+        print("Initiating remote driver on platfrom: "+desired_cap["platform"]+" browser: "+desired_cap["browserName"]+" version: "+desired_cap["version"])
         self.driver = webdriver.Remote(
             desired_capabilities=desired_cap,
             command_executor= url
         )
 
-    """
-    Setup remote driver
-    Params
-    ----------
-    Execute test:  navigate google.com search LambdaTest
-    Result
-    -------
-    print title
-    """
+    
     def test_search_in_google(self):
+        """
+        Setup remote driver
+        Params
+        ----------
+        Execute test:  navigate google.com search LambdaTest
+        Result
+        -------
+        print title
+        """
         driver = self.driver
-        print("Driver initiated successfully.  Navigate url")
+        print("Driver initiated sucessfully.  Navigate url")
         driver.get("https://www.google.com/ncr")
 
         print("Searching lambdatest on google.com ")
@@ -83,10 +86,11 @@ class LTAutomate(unittest.TestCase):
         driver.execute_script("lambda-status=passed")
         print("Requesting to mark test : pass")
 
-    """
-    Quit selenium driver
-    """
+    
     def tearDown(self):
+        """
+        Quit selenium driver
+        """
         self.driver.quit()
 
 if __name__ == "__main__":
