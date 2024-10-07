4ba8b2b1bb4b8fc98ddd01bb71d8682e62c7d23d
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2024-10-07 12:34:09 +0000

    selenium 3 example

--- selenium-3/lambdatest.py
+++ selenium-3/lambdatest.py
@@ -3,61 +3,53 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import warnings
 
 username = os.getenv("LT_USERNAME")  # Replace the username
 access_key = os.getenv("LT_ACCESS_KEY")  # Replace the access key
 
 
-# paste your capibility options below
+capability = {
+	"browserName": "Chrome",
+	"browserVersion": "latest",
+	"LT:Options": {
+        "username": username,
+		"accessKey": access_key,
+		"platformName": "Windows 10",
+        "build": "Selenium 3 Example",
+        "name" : "Selenium 3 Sample Test"
+	}
+}
 
-options = ChromeOptions()
-options.browser_version = "latest"
-options.platform_name = "Windows 10"
-lt_options = {}
-lt_options["username"] = username
-lt_options["accessKey"] = access_key
-# lt_options["video"] = True
-# lt_options["resolution"] = "1920x1080"
-lt_options["network"] = True
-lt_options["build"] = "Selenium 4 Example"
-# lt_options["project"] = "unit_testing"
-lt_options["smartUI.project"] = "test"
-lt_options["name"] = "Selenium 4 Sample Test"
-lt_options["w3c"] = True
-lt_options["plugin"] = "python-python"
-options.set_capability("LT:Options", lt_options)
-
-
-# Steps to run Smart UI project (https://beta-smartui.lambdatest.com/)
-# Step - 1 : Change the hub URL to @beta-smartui-hub.lambdatest.com/wd/hub
-# Step - 2 : Add "smartUI.project": "<Project Name>" as a capability above
-# Step - 3 : Run "driver.execute_script("smartui.takeScreenshot")" command wherever you need to take a screenshot
-# Note: for additional capabilities navigate to https://www.lambdatest.com/support/docs/test-settings-options/
-
+def suppress_resource_warnings():
+    warnings.filterwarnings("ignore", category=ResourceWarning)
 
 class FirstSampleTest(unittest.TestCase):
     driver = None
 
     def setUp(self):
+        suppress_resource_warnings()
         self.driver = webdriver.Remote(
-            command_executor="http://{}:{}@hub.lambdatest.com/wd/hub".format(
+            command_executor = "http://{}:{}@hub.lambdatest.com/wd/hub".format(
                 username, access_key
             ),
-            options=options,
+            # options=options,
+            desired_capabilities = capability
         )
 
     # """ You can write the test cases here """
     def test_demo_site(self):
-        # try:
+        suppress_resource_warnings()
         driver = self.driver
         driver.implicitly_wait(10)
         driver.set_page_load_timeout(30)
-        driver.set_window_size(1920, 1080)
+        # driver.set_window_size(1920, 1080)
 
         # Url
         print("Loading URL")
         driver.get(
-            "https://stage-lambda-devops-use-only.lambdatestinternal.com/To-do-app/index.html"
+            "https://lambdatest.github.io/sample-todo-app/"
         )
 
         # Let's click on a element
@@ -65,9 +57,6 @@ class FirstSampleTest(unittest.TestCase):
         location = driver.find_element(By.NAME, "li2")
         location.click()
         print("Clicked on the second element")
-
-        # Take Smart UI screenshot
-        # driver.execute_script("smartui.takeScreenshot")
 
         # Let's add a checkbox
         driver.find_element(By.ID, "sampletodotext").send_keys("LambdaTest")
@@ -82,7 +71,7 @@ class FirstSampleTest(unittest.TestCase):
         search.click()
         driver.implicitly_wait(3)
 
-        # Let's download the invoice
+         # Let's download the invoice
         heading = driver.find_element(By.CSS_SELECTOR, ".container h2")
         if heading.is_displayed():
             heading.click()
@@ -91,10 +80,10 @@ class FirstSampleTest(unittest.TestCase):
         else:
             driver.execute_script("lambda-status=failed")
 
-    # tearDown runs after each test case
+  # tearDown runs after each test case
     def tearDown(self):
         self.driver.quit()
-
+       
 
 if __name__ == "__main__":
     unittest.main()
